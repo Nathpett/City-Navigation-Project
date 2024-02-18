c89395eb619dcaaf85b5860e67ddfdb2749a76cb
Nathaniel Pettit <89318769+Nathpett@users.noreply.github.com>
2024-02-17 21:19:02 -0500

    redid file thing

--- state/state_machine.gd
+++ state/state_machine.gd
@@ -13,7 +13,8 @@ func push_state(state_name, parameters = null, overwrite = false) -> void:
 			state.pause()
 			state_stack.append(state)
 	state = Node.new()
-	state.set_script(load("res://state/%s.gd" % [state_name]))
+	var _script = _get_state_script(state_name)
+	state.set_script(_script)
 	state.state_owner = get_parent()
 	state.push_state = Callable(self, "push_state")
 	state.connect("state_concluded", Callable(self, "_on_state_concluded"))
@@ -28,3 +29,14 @@ func _on_state_concluded() -> void:
 		return
 	state = state_stack.pop_back()
 	state.unpause()
+
+
+func _get_state_script(state_name: String):
+	var state_dir = DirAccess.open("res://state")
+	
+	for dir_name in state_dir.get_directories():
+		var dir = DirAccess.open("res://state/%s" % dir_name)
+		var script_path = "res://state/%s/%s.gd" % [dir_name, state_name]
+		if dir.file_exists(script_path):
+			return load(script_path)
+	return ""
