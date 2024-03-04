549aec905375908c6646c1f0e2ce29597828fa8b
Nathaniel Pettit <89318769+Nathpett@users.noreply.github.com>
2024-03-03 20:11:41 -0500

    player

--- things/player.gd
+++ things/player.gd
@@ -1,2 +1,18 @@
 extends Character
 
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("up"):
+		direction += Vector2.UP
+	if Input.is_action_pressed("down"):
+		direction += Vector2.DOWN
+	if Input.is_action_pressed("right"):
+		direction += Vector2.RIGHT
+	if Input.is_action_pressed("left"):
+		direction += Vector2.LEFT
+	
+	velocity = direction * movement_speed
+	
+	move_and_slide()
